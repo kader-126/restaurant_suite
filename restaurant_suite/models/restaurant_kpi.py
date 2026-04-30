21166f4d43f9c21c646bb0acf53af2a9a9988c82
kbkader23 <kbkader126@gmail.com>
2026-04-30 20:06:01 +0200

    fix(kpi): enable log_access for transient model

--- restaurant_suite/models/restaurant_kpi.py
+++ restaurant_suite/models/restaurant_kpi.py
@@ -6,6 +6,7 @@ class RestaurantKPI(models.Model):
     _description = 'Restaurant KPI Engine'
     _auto = False
     _transient = True
+    _log_access = True
 
     @api.model
     def get_dashboard_data(self, date_from, date_to, company_id=None):
